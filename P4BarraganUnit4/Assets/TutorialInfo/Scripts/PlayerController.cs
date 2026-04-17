357b5f4e51e5ba9f36f26ac887fdf05350654e92
728843-ui <728843@student.dusd.net>
2026-04-17 15:23:57 -0700

    efefew

--- P4BarraganUnit4/Assets/TutorialInfo/Scripts/PlayerController.cs
+++ P4BarraganUnit4/Assets/TutorialInfo/Scripts/PlayerController.cs
@@ -3,19 +3,21 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private GameObject focalPoint;
     public float speed = 5.0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
     }
 
     // Update is called once per frame
     void Update()
     {
-        float fowardInput = Input.GetAxis("Vertical");
+        float forwardInput = Input.GetAxis("Vertical");
 
-        playerRb.AddForce(Vector3.forward * fowardInput * speed);
+        playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
     }
 }
